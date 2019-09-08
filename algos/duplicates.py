a6d2f891f81317bfeeb05c3896bdf100b33ea9f5
Jacinda Zhong <jacindaz@gmail.com>
2019-09-08 14:53:26 -0700

    Duplicates, many different solutions + tests

--- algos/duplicates.py
+++ algos/duplicates.py
@@ -3,14 +3,32 @@ import random
 import time
 
 
+def have_duplicates_n_squared(array):
+    for index, item in enumerate(array):
+        for index2, item2 in enumerate(array[(index+1):]):
+            if item == item2:
+                return True
+    return False
+
+
 def have_duplicates1(array):
     """ O(n) solution """
     result = Counter(array)
-    return len(array) == len(result)
+    return len(array) != len(result)
+
+
+def have_duplicates_set(array):
+    """ O(n) solution """
+    result = set(array)
+    return len(array) != len(result)
+
 
 def have_duplicates2(array):
+    """
+    O(n) solution, same as above, but builds
+    dictionary manually.
+    """
     result = {}
-
     for char in array:
         if result.get(char):
             return True
@@ -19,8 +37,7 @@ def have_duplicates2(array):
     return False
 
 
-
-def have_duplicates2(array):
+def have_duplicates3(array):
     """ O(n log n) solution """
     array_sorted = sorted(array)
     previous_item = array_sorted[0]
@@ -28,29 +45,25 @@ def have_duplicates2(array):
         # if seen prevoius item
         if index > 0 and previous_item == item:
             return True
-
     return False
-
-# print(have_duplicates2([1,2,1]))
-# print(have_duplicates2([1,2,3]))
 
 
 def timing(list_length):
     total = 0
-    for _ in range(list_length):
-        start = time.time()
+    random_num_list = list(range(list_length))
 
-        random_num_list = [n for n in range(list_length)]
-        # have_duplicates2(random_num_list)
-        end = time.time()
-        total += (end-start)
+    start = time.time()
+    have_duplicates3(random_num_list)
+    end = time.time()
+
+    total += (end-start)
 
     print(f"num_times ran: {list_length}, total: {total}, avg: {total/list_length}")
 
 
-timing(100)
-timing(1000)
-timing(10000)
-timing(100000)
-timing(1000000)
-timing(10000000)
+# timing(100)
+# timing(1000)
+# timing(10000)
+# timing(100000)
+# timing(1000000)
+# timing(10000000)
